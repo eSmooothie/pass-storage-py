539f1f329c92034a60d0a5630ba7d54796c91480
Sherwin <sherwin.sandoval026@gmail.com>
2022-04-11 20:05:33 +0800

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+import argparse
+import sys
+
+if __name__ == "__main__":
+    print("Hello")
+
+    print("args: {0}".format(sys.argv))
---
+++ storepass.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+C:\Python310\python.exe D:\Development\Others\Python\pass-storage-py\main.py %*
